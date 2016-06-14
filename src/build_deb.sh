793cb20e3f7525795be5b6f2b7c296f9f9cb454d
HOU ChG <chg.hou@gmail.com>
2016-06-14 22:42:08 +0800

    deb package

---
+++ src/build_deb.sh
@@ -0,0 +1,5 @@
+python3 setup.py bdist
+
+python3 setup.py --command-packages=stdeb.command bdist_deb
+
+
--- src/setup.py
+++ src/setup.py
@@ -1,13 +1,31 @@
-from distutils.core import setup
+
+try:
+    from setuptools import setup
+except ImportError:
+    from distutils.core import setup
+
 
 setup(
     name='DawnlightSearch',
     version='0.1',
-    packages=['', 'DB_Builder', 'MFT_parser', 'QueryWorker', 'UI_delegate'],
-    package_dir={'': 'DawnlightSearch'},
+    packages=['DawnlightSearch'],
+    package_dir={'DawnlightSearch': 'DawnlightSearch'},
     url='https://github.com/chg-hou/DawnlightSearch',
     license='GPL2',
     author='chg-hou',
     author_email='chg.hou@gmail.com',
-    description='A Linux version of Everything Search Engine.'
+    description='A Linux version of Everything Search Engine.',
+
+    package_data={'DawnlightSearch': ['*.desktop','ui/icon/*.png', 'MFT_parser/mft_cpp_parser','*.ui',
+                                      'DB_Builder/*.py', 'MFT_parser/*.py','QueryWorker/*.py',
+                                      'UI_delegate/*.py']},
+    install_requires=['PyQt5'],
+    data_files=[('share/applications',  ['DawnlightSearch/DawnlightSearch.desktop']),
+                ('share/pixmaps',       ['DawnlightSearch/ui/icon/DawnlightSearch.png'])],
+
 )
+
+
+# depedence: PyQt5          python3-pyqt5
+# xdg-utils
+# python3-gi
