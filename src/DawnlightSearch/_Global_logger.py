d7bb3280b53c4599bbbbb3fecfee517cb61f8305
ChG.Hou <chghou@gmail.com>
2018-06-04 20:03:30 +0800

    delete src dir

--- src/DawnlightSearch/_Global_logger.py
+++
@@ -1,8 +0,0 @@
-from __future__ import absolute_import
-
-import logging
-import logging.handlers
-
-logging.basicConfig(format='%(asctime)s - %(thread)d:%(funcName)s:%(lineno)s - %(name)s - %(message)s')
-logger = logging.getLogger()
-logger.setLevel(logging.DEBUG)
--- src/DawnlightSearch/__init__.py
+++
@@ -1,1 +0,0 @@
-__all__ = ["_Global_DawnlightSearch", "_Global_Qt_import", "DawnlightSearch", "update_db_7"]
